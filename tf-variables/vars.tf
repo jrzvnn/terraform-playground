76b027228044b4b174bc6bb42e01d52814fcb630
jrzvnn <jrzvnn@gmail.com>
2023-08-25 13:12:46 +0800

    Launch ec2 using variables on terraform

---
+++ tf-variables/vars.tf
@@ -0,0 +1,16 @@
+variable "AWS_ACCESS_KEY" {
+}
+
+variable "AWS_SECRET_KEY" {
+}
+
+variable "AWS_REGION" {
+  default = "ap-southeast-2"
+}
+
+variable "AMIS" {
+  type = map(string)
+  default = {
+    ap-southeast-2 = "ami-0a709bebf4fa9246f"
+  }
+}
