854a8bdc9ccb3ee85be67d3505068a92654aaf6f
jrzvnn <jrzvnn@gmail.com>
2023-08-26 12:06:09 +0800

    Added config to launch an EC2 with windows server provisioning

---
+++ windows-server-provisioning/vars.tf
@@ -0,0 +1,29 @@
+variable "AWS_ACCESS_KEY" {
+
+}
+
+variable "AWS_SECRET_KEY" {
+
+}
+
+variable "AWS_REGION" {
+  default = "ap-southeast-2"
+}
+
+variable "PATH_TO_PRIVATE_KEY" {
+  default = "mykey"
+}
+
+variable "PATH_TO_PUBLIC_KEY" {
+  default = "mykey.pub"
+}
+
+variable "INSTANCE_USERNAME" {
+  default = "Terraform"
+}
+
+variable "INSTANCE_PASSWORD" {
+}
+
+
+
