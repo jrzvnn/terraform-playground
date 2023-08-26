c88191dc6d7be73a165012e31e42bd4639f68a34
jrzvnn <jrzvnn@gmail.com>
2023-08-26 15:34:36 +0800

    Added config to launch EC2 with consul module

---
+++ external-modules/vars.tf
@@ -0,0 +1,17 @@
+variable "AWS_ACCESS_KEY" {
+}
+
+variable "AWS_SECRET_KEY" {
+}
+
+variable "AWS_REGION" { 
+  default = "ap-southeast-2"
+}
+
+variable "PATH_TO_PRIVATE_KEY" {
+  default = "mykey"
+}
+
+variable "PATH_TO_PUBLIC_KEY" {
+  default = "mykey.pub"
+}
